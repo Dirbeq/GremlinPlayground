a0c42f407aacc0878e9a2521380ed72b34cf6410
corentin <corentin.pannier@eulerts.com>
2023-06-08 15:45:11 +0200

    aaaaaaaa

--- LOF/SKLearnLocalOutlierFactor.py
+++ LOF/SKLearnLocalOutlierFactor.py
@@ -11,18 +11,18 @@ def generate_data(g):
     # Generate data
     g.addV('Person').property('name', 'Alice').property('age', 30).property('nb_client', 5).next()
     g.addV('Person').property('name', 'Bob').property('age', 27).property('nb_client', 6).next()
-    g.addV('Person').property('name', 'Charlie').property('age', 32).property('nb_client', 7).next()
-    g.addV('Person').property('name', 'Diana').property('age', 39).property('nb_client', 8).next()
+    g.addV('Person').property('name', 'Charlie').property('age', -32).property('nb_client', 7).next()
+    g.addV('Person').property('name', 'Diana').property('age', 39).property('nb_client', -8).next()
     g.addV('Person').property('name', 'Eve').property('age', 20).property('nb_client', 4).next()
     g.addV('Person').property('name', 'Frank').property('age', 80).property('nb_client', 30).next()
     g.addV('Person').property('name', 'Grace').property('age', 2).property('nb_client', -20).next()
     g.addV('Person').property('name', 'Helen').property('age', 25).property('nb_client', 9).next()
     g.addV('Person').property('name', 'Ivan').property('age', 33).property('nb_client', 7).next()
-    g.addV('Person').property('name', 'Julia').property('age', 28).property('nb_client', 3).next()
+    g.addV('Person').property('name', 'Julia').property('age', -28).property('nb_client', -3).next()
     g.addV('Person').property('name', 'Kevin').property('age', 31).property('nb_client', 5).next()
     g.addV('Person').property('name', 'Linda').property('age', 40).property('nb_client', 60).next()
     g.addV('Person').property('name', 'Michael').property('age', 35).property('nb_client', 7).next()
-    g.addV('Person').property('name', 'Nancy').property('age', 32).property('nb_client', 8).next()
+    g.addV('Person').property('name', 'Nancy').property('age', -35).property('nb_client', 8).next()
     g.addV('Person').property('name', 'Olivia').property('age', 30).property('nb_client', 5).next()
     g.addV('Person').property('name', 'Peter').property('age', -32).property('nb_client', 50).next()
     g.addV('Person').property('name', 'Quentin').property('age', -30).property('nb_client', 6).next()
@@ -54,13 +54,14 @@ X = [[v['nb_client'][0], v['age'][0]] for v in vertices]
 
 # Perform LOF analysis
 n_neighbors = min(10, len(vertices) - 1)  # Adjust the value based on your needs
-lof = LocalOutlierFactor(n_neighbors=n_neighbors, contamination=0.12)
+contamination = 0.12  # Adjust the value based on your needs
+lof = LocalOutlierFactor(n_neighbors=n_neighbors, contamination=contamination)
 y_pred = lof.fit_predict(X)
 lof_scores = -lof.negative_outlier_factor_
 
 # Perform Isolation Forest analysis
 n_estimators = 100  # Adjust the value based on your needs
-isolation_forest = IsolationForest(n_estimators=n_estimators, contamination=0.12)
+isolation_forest = IsolationForest(n_estimators=n_estimators, contamination=contamination)
 isolation_forest.fit(X)
 if_scores = isolation_forest.decision_function(X)
 

d2573b74660a9557124fd512b68ef174ff3c26f3
corentin <corentin.pannier@eulerts.com>
2023-06-09 12:17:37 +0200

    main main main

--- AnomalyDetection/IsolationForest/IF.py
+++ AnomalyDetection/IsolationForest/IF.py
@@ -3,23 +3,29 @@ from sklearn.ensemble import IsolationForest
 
 from AnomalyDetection.utilsAD import generate_random_data
 
-# Generate random data with outliers
-n_samples = 1000
-n_features = 2
-random_state = 0
-X_train = generate_random_data(n_samples, n_features, random_state=random_state)
 
-# Fit the Isolation Forest model
-clf = IsolationForest(random_state=random_state)
-clf.fit(X_train)
+def main():
+    # Generate random data with outliers
+    n_samples = 1000
+    n_features = 2
+    random_state = 42
+    X_train = generate_random_data(n_samples, n_features, random_state=random_state)
 
-# Predict anomaly scores for training data
-scores_train = clf.decision_function(X_train)
+    # Fit the Isolation Forest model
+    clf = IsolationForest(random_state=random_state)
+    clf.fit(X_train)
 
-# Plot the training data with anomaly scores
-plt.scatter(X_train[:, 0], X_train[:, 1], c=-scores_train, cmap='viridis')
-plt.colorbar(label='Anomaly Score')
-plt.xlabel('Feature 1')
-plt.ylabel('Feature 2')
-plt.title('Isolation Forest Anomaly Detection')
-plt.show()
+    # Predict anomaly scores for training data
+    scores_train = clf.decision_function(X_train)
+
+    # Plot the training data with anomaly scores
+    plt.scatter(X_train[:, 0], X_train[:, 1], c=-scores_train, cmap='viridis')
+    plt.colorbar(label='Anomaly Score')
+    plt.xlabel('Feature 1')
+    plt.ylabel('Feature 2')
+    plt.title('Isolation Forest Anomaly Detection')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
--- AnomalyDetection/LocalOutlierFactor/LOF.py
+++ AnomalyDetection/LocalOutlierFactor/LOF.py
@@ -3,20 +3,26 @@ from sklearn.neighbors import LocalOutlierFactor
 
 from AnomalyDetection.utilsAD import generate_random_data
 
-# Generate random data with outliers
-n_samples = 1000
-n_features = 2
-random_state = 42
-X_train = generate_random_data(n_samples, n_features, random_state=random_state)
 
-# Fit the LOF model
-clf = LocalOutlierFactor()
-y_pred = clf.fit_predict(X_train)
+def main():
+    # Generate random data with outliers
+    n_samples = 1000
+    n_features = 2
+    random_state = 42
+    X_train = generate_random_data(n_samples, n_features, random_state=random_state)
 
-# Plot the training data with outlier labels
-plt.scatter(X_train[:, 0], X_train[:, 1], c=y_pred, cmap='viridis')
-plt.colorbar(label='Outlier Label')
-plt.xlabel('Feature 1')
-plt.ylabel('Feature 2')
-plt.title('Local Outlier Factor')
-plt.show()
+    # Fit the LOF model
+    clf = LocalOutlierFactor()
+    y_pred = clf.fit_predict(X_train)
+
+    # Plot the training data with outlier labels
+    plt.scatter(X_train[:, 0], X_train[:, 1], c=y_pred, cmap='viridis')
+    plt.colorbar(label='Outlier Label')
+    plt.xlabel('Feature 1')
+    plt.ylabel('Feature 2')
+    plt.title('Local Outlier Factor')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()

4f82a384a40c78cae78e6da9b33415a28cbd59b6
corentin <corentin.pannier@eulerts.com>
2023-06-09 12:23:26 +0200

    mainAD

---
+++ AnomalyDetection/mainAD.py
@@ -0,0 +1,11 @@
+from IsolationForest.IF import main as IFmain
+from LocalOutlierFactor.LOF import main as LOFmain
+
+
+def main():
+    IFmain()
+    LOFmain()
+
+
+if __name__ == '__main__':
+    main()

4a016fa409b93724f1d280bf48e6f9526214d5b4
corentin <corentin.pannier@eulerts.com>
2023-06-09 16:02:12 +0200

    typo

--- AnomalyDetection/utilsAD.py
+++ AnomalyDetection/utilsAD.py
@@ -6,11 +6,11 @@ def generate_random_data(n_samples, n_features, random_state=None):
     if random_state is not None:
         np.random.seed(random_state)
 
-    centers = [[-4, -4], [4, 4]]
+    centers = [[-4, -4], [4, 4], [8, 8]]
     x_train, _ = make_blobs(n_samples=n_samples, centers=centers, random_state=random_state)
 
     # Generate outliers
-    n_outliers = int(n_samples * 0.1)  # 10% of samples as outliers
+    n_outliers = int(n_samples * 0.05)  # % of samples as outliers
     x_outliers = np.random.uniform(low=-10, high=10, size=(n_outliers, n_features))
     x_train = np.concatenate((x_train, x_outliers), axis=0)
 

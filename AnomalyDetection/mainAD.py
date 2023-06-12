8ae867a256f78fd602319c723a3b320d64a0ebd5
corentin <corentin.pannier@eulerts.com>
2023-06-12 16:05:11 +0200

    Changed inne to this own file due to correct outlier detection

--- AnomalyDetection/mainAD.py
+++ AnomalyDetection/mainAD.py
@@ -1,9 +1,9 @@
+from AnomalyDetection.IsolationNearestNeighborsEnsemble.INNE import main as INNEmain
 from IsolationForest.IF import main as IFmain
 from LocalOutlierFactor.LOF import main as LOFmain
 from OneClassSVM.OCSvm import main as OCSVMmain
 from Other.COPOD import main as COPODmain
 from Other.GMM import main as GMMmain
-from Other.INNE import main as INNEmain
 
 
 def main():
